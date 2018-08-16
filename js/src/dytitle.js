5c873b94266a72d370735e317689b3fa99e01628
BlueMrD <2361018131@qq.com>
2018-08-16 12:06:49 +0800

    Site updated: 2018-08-16 12:06:47

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -1,1 +1,18 @@
-"use strict";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/images/favicon.png"),document.title=" 你瞒 ~ | BlueMrD！",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/images/favicon.png"),document.title=" 我瞒~ "+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+<!--崩溃欺骗-->
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/images/favicon.png");
+         document.title = ' 你瞒 ~ | BlueMrD！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/images/favicon.png");
+         document.title = ' 我瞒~ ' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
+
